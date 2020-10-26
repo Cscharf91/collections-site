b1455a364fd57c1fe2e832e4c2798999b5fb9b51
Cscharf91 <cscharf91@gmail.com>
2020-10-26 15:48:12 -0400

    added back ES6 JS and updated uglifier

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,9 +1,8 @@
-window.addEventListener('turbolinks:load', function() {
-    console.log('hi!');
+window.addEventListener('turbolinks:load', () => {
     const menu = document.querySelector('.mobile-menu');
     const grid = document.querySelector('.nav-grid');
     const navList = document.querySelector('.nav-list')
-    menu.addEventListener('click', function() {
+    menu.addEventListener('click', () => {
         console.log('clicked!');
         menu.classList.toggle('active-menu');
         navList.classList.toggle('hidden-menu');
